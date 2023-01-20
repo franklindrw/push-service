51e6c6b6c3dbf55af91eb92c36bbc963b2505848
franklindrw <franklindrw@gmail.com>
2023-01-20 02:17:03 -0300

    adicionando os testes cancel-notification

---
+++ src/app/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,41 @@
+import { Content } from '@app/entities/content';
+import { Notification } from '@app/entities/notification';
+import { CancelNotification } from './cancel-notification';
+import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notifications-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('Cancel notification', () => {
+  it('should be able to send notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const cancelNotification = new CancelNotification(notificationRepository);
+
+    const notification = new Notification({
+      category: 'social',
+      content: new Content('Nova solicitação de amizade!'),
+      recipientId: 'example-notification-id',
+    });
+
+    await notificationRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    console.log(notificationRepository.notifications);
+
+    expect(notificationRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a notification when it does not exist', () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const cancelNotification = new CancelNotification(notificationRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
